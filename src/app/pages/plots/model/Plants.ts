9221f32cd68185c195ab114abf80aa03ea99b2c3
LudwinReyes <89177248+LudwinReyes@users.noreply.github.com>
2022-10-21 20:56:04 -0500

    feat: Added section plots

---
+++ src/app/pages/plots/model/Plants.ts
@@ -0,0 +1,17 @@
+export interface Plants{
+    id: number;
+    name: string;
+    image: string;
+    scientifistname: string;
+    variety: string;
+    infolandType: string;
+    ph: number;
+    infoDistanceBetween: string;
+    distancePlants: string;
+    infoIdealDepth: string;
+    depth: string;
+    infoWeatherConditions: string;
+    weather: string;
+    infoFertFumig: string;
+    savePlant: boolean;
+}
