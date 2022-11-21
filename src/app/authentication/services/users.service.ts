5a6f9d6970cf3095d8767441a15444a45ff2ba10
SgtGarro <113218617+MarceloGarro22@users.noreply.github.com>
2022-11-21 16:02:04 -0500

    feat: Auth implemented with API

--- src/app/authentication/services/users.service.ts
+++ src/app/authentication/services/users.service.ts
@@ -1,15 +1,14 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TemplateService } from "src/app/services/template.service";
-import { Users } from "../log-in/model/users";
+import { TemplateService } from 'src/app/services/template.service';
+import { User } from '../model/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UsersService extends TemplateService<Users> {
-
+export class UsersService extends TemplateService<User> {
   constructor(http: HttpClient) {
     super(http);
-    this.basePath = 'http://localhost:3000/users';
+    this.basePath = 'http://localhost:8080/api/users';
   }
 }
